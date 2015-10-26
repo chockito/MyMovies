9e5254a0cdbe1ef6f4968a342818ecf3629a0d4f
chockito <chock_it@web.de>
2015-10-26 16:40:47 +0100

    Testing code first commit

--- OCA/src/ImportExample.java
+++ OCA/src/ImportExample.java
@@ -1,11 +1,18 @@
 
 import java.util.Random;
+import java.util.Date;
+import java.sql.*;
 
 public class ImportExample {
 
 		public static void main(String[] args) {
-		Random r = new Random(); // DOES NOT COMPILE
-		System.out.println(r.nextInt(10));
+			
+			for(String arg : args ) {
+				System.out.println("->"+arg);
+			}
+			
+			Random r = new Random(); // DOES NOT COMPILE
+			System.out.println(r.nextInt(10));
 		}
 	
 }
---
+++ OCA/src/Water.java
@@ -0,0 +1,16 @@
+
+public class Water {
+ private boolean empty;
+ private String brand;
+ 
+ public static void main (String[] arg) {
+	 Water w=new Water();
+	 
+	 System.out.print(w.empty);
+	 System.out.println(" "+w.brand);
+	 
+	 if (true) {} 
+	 int x=1;
+	 System.out.println(x);
+ }
+}
